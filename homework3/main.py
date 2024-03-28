b6b0fc9d688cf7658822e0f7d527f70085972204
Ana-Maria <95433627+Ana-Maria-C@users.noreply.github.com>
2024-03-28 13:01:23 +0200

    +bonus tema3

--- homework3/main.py
+++ homework3/main.py
@@ -6,7 +6,7 @@ def is_zero_with_precision(value, eps):
 
 
 # Ex 1
-def calcul_vector_b(A,s):
+def calcul_vector_b(A, s):
     n = len(s)
     b = np.zeros(n)
     for i in range(n):
@@ -24,6 +24,11 @@ def transposition(A):
     return B
 
 
+def determinant_using_qr(Q, R):
+    determinant_A = np.prod(np.diag(R))
+    return determinant_A
+
+
 # Ex 2
 def householder_QR(A, epsilon):
     n = len(A)
@@ -36,7 +41,7 @@ def householder_QR(A, epsilon):
         H = np.identity(n)
         for j in range(len(v)):
             for k in range(len(v)):
-                if not is_zero_with_precision(np.dot(v, v), epsilon) :
+                if not is_zero_with_precision(np.dot(v, v), epsilon):
                     H[i + j][i + k] -= 2 * v[j] * v[k] / np.dot(v, v)
         Q = np.dot(Q, H)
         A = np.dot(H, A)
@@ -59,27 +64,45 @@ def solve_linear_system(Q, R, b):
 
 
 def norm(x, y):
-    return np.linalg.norm(x-y)
+    return np.linalg.norm(x - y)
 
 
 # Ex 5
-def inverse_using_qr(Q, R):
-    n = Q.shape[0]
-    inverse = np.zeros((n, n))
+def inverse_using_qr(Q, R, epsilon):
+    if is_zero_with_precision(determinant_using_qr(Q, R), epsilon):
+        print("Matricea nu este inversabila")
+        return None
+    else:
+        n = Q.shape[0]
+        inverse = np.zeros((n, n))
 
-    # Rezolvăm sistemul pentru fiecare coloană a matricei inverse
-    for i in range(n):
-        Qt = transposition(Q)
-        b = np.zeros(n)
-        b[i] = 1  # Vectorul de pe coloana i a matricei identitate
-        Qt_b = np.dot(Qt, b)  # Q^T * b
-        x = np.linalg.solve(R, Qt_b)  # Rezolvăm sistemul Rx = Q^T * b pentru fiecare coloană
-        inverse[:, i] = x
+        # Rezolvam sistemul pentru fiecare coloana a matricei inverse
+        for i in range(n):
+            Qt = transposition(Q)
+            b = np.zeros(n)
+            b[i] = 1  # Vectorul de pe coloana i a matricei identitate
+            Qt_b = np.dot(Qt, b)  # Q^T * b
+            x = np.linalg.solve(R, Qt_b)  # Rezolvam sistemul Rx = Q^T * b pentru fiecare coloana
+            inverse[:, i] = x
 
-    return inverse
+        return inverse
 
 
-def afisare(A,s,epsilon):
+def qr_algorithm(A, epsilon):
+    Ak = np.copy(A)
+    while True:
+        Q, R = householder_QR(Ak, epsilon)
+        # Folosim doar elementele din partea superioara triunghiulara a matricei R (rij cu i≤j)
+        R_upper_triangular = np.triu(R)
+        Ak_next = np.dot(R_upper_triangular, Q)
+        print(np.linalg.norm(Ak_next - Ak))
+        if np.linalg.norm(Ak_next - Ak) <= epsilon:
+            break
+        Ak = Ak_next
+    return Ak_next
+
+
+def afisare(A, s, epsilon):
     A_init = A
     print("Ex 1")
     print("vectorul b:")
@@ -111,23 +134,27 @@ def afisare(A,s,epsilon):
 
     # Ex 5
     print("Ex 5")
-    A_inverse = inverse_using_qr(Q, R)
+    A_inverse = inverse_using_qr(Q, R, epsilon)
     print("A_inverse:")
     print(A_inverse)
     A_inverse_numpy = np.linalg.inv(A)
     print("A_inverse_numpy:")
     print(A_inverse_numpy)
     print("|| A_inverse - A_inverse_numpy || = ", norm(A_inverse, A_inverse_numpy))
+    print()
+    print("Ex 6")
+    B = np.array([[0, 0], [1, 2]])
+    print("B_k = ", qr_algorithm(B, epsilon))
+    print("A_k = ", qr_algorithm(A_init, epsilon))
 
 
 def main():
-
     # t = int(input("Introduceti precizia pentru verificarea impartirii la zero: "))
-    t=5
+    t = 5
     epsilon = 10 ** (-t)
 
-    # Testare pe matricea data in enunt
-    A = np.array([[0, 0, 4], [1, 2, 3], [0, 1, 2]])
+    # Matrice simetrica cu valori mici pentru a testa bonusul
+    A = np.array([[2, -1, 0], [-1, 2, -1], [0, -1, 2]])
     s = np.array([3, 2, 1])
 
     # Testare pe o matrice de dimensiune mai mare
@@ -136,5 +163,6 @@ def main():
     s_large = np.random.rand(n)
 
     afisare(A, s, epsilon)
+
 
 main()
